51bf38b7b8cf76919d86c1d7c01a741114e4e159
Pandemic Moon <pandemic@moon-network.net>
2017-03-26 13:54:02 -0400

    Updated README and added socket.io

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 An open source live blogging application built with Node.js, Express, and Socket.io.
 
+## Note
+
+This is a **work in progress**, meaning it isn't close to a stable release. *At this moment, it provides no functionality whatsoever.*
+
 ### Set Up
+
 ```shell
 git clone https://github.com/PandemicMoon/live-blogging.git
 cd live-blogging && npm install
--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var debug = require('debug')('project-view-server:server');
+var http = require('http');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,7 +20,10 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded(
+{
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,35 +31,141 @@ app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function(req, res, next)
+{
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+if (app.get('env') === 'development')
+{
+    app.use(function(err, req, res, next)
+    {
+        res.status(err.status || 500);
+        res.render('error',
+        {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function(err, req, res, next)
+{
+    res.status(err.status || 500);
+    res.render('error',
+    {
+        message: err.message,
+        error:
+        {}
+    });
 });
 
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || '5000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Create Socket server.
+ */
+
+var io = require('socket.io')(server);
+
+io.on('connection', function(client)
+{
+    client.on('disconnect', function()
+    {
+    });
+});
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val)
+{
+    var port = parseInt(val, 10);
+
+    if (isNaN(port))
+    {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0)
+    {
+        // port number
+        return port;
+    }
+
+    return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error)
+{
+    if (error.syscall !== 'listen')
+    {
+        throw error;
+    }
+
+    var bind = typeof port === 'string' ?
+        'Pipe ' + port :
+        'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code)
+    {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening()
+{
+    var addr = server.address();
+    var bind = typeof addr === 'string' ?
+        'pipe ' + addr :
+        'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
 
 module.exports = app;
